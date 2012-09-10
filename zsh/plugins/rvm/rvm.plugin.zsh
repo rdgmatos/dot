3ea7911fff39971bf2c1d27fa56669dc0e8f01ca
pahagon <paulo.ahagon@gmail.com>
2012-09-10 13:25:55 -0300

    Removed zsh dir and add submodule for oh-my-zsh

--- zsh/plugins/rvm/rvm.plugin.zsh
+++
@@ -1,8 +0,0 @@
-if [ -f ~/.rvm/scripts/rvm ]
-then
-  source ~/.rvm/scripts/rvm
-else
-  echo "[rvm plugin] No default ruby found found, please type: "
-  echo "[rvm plugin] $ rvm --default use x.x.x"
-  echo "[rvm plugin] where x.x.x is the version of ruby you want to use to define one."
-fi
--- zsh/uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-if [ -h $HOME/.oh-my-zsh ] 
-  then unlink $HOME/.oh-my-zsh;
-fi
-
-if [ -h $HOME/.zshrc ]
-  then ln s $HOME/.zshrc;
-fi
