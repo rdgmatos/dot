80d40a6ceabd9878a4f8101ebfb35b57236dfbb8
pahagon <paulo.ahagon@gmail.com>
2012-09-18 20:27:23 -0300

    Remove screen configurations.

--- screen/install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-if [ ! -h $HOME/.screenrc ]
-   then ln -s "$(pwd)/screenrc" $HOME/.screenrc;
-fi
--- screen/uninstall.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh
-if [ -h $HOME/.screenrc ] 
-  then unlink $HOME/.screenrc;
-fi
